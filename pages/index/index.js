8c7f8d36f958ebbf98396ed3c584dbf7344119bd
zxl <lyon1890@163.com>
2019-10-24 08:19:41 +0800

    项目初始化

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    tmpList: [
+      { name: '肯德基', num: 20 },
+      { name: '麦当劳', num: 12 },
+      { name: '必胜客', num: 8 },
+      { name: '尊宝披萨', num: 11 },
+      { name: '美团外卖', num: 2 },
+      { name: '饿了么', num: 15 },
+    ]
+  }
+})
